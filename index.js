30d289c1b4a80331f47387df9cd0f0f7f1559cf0
Vitor Bruno <vitorbrunorj@gmail.com>
2023-11-06 19:14:49 -0300

    pagina de pergunta finalizada

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/perguntar', (req, res) => {
+  res.render('perguntar');
+});
+
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
 });
