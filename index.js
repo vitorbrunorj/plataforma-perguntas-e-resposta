76fe03d23ebf550842d9e71aa8617d2dff7ea66c
Vitor Bruno <vitorbrunorj@gmail.com>
2023-11-06 18:12:44 -0300

    config

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
