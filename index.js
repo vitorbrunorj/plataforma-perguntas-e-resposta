be80deae7d70859203263619cd1797ae52ec43cb
Vitor Bruno <vitorbrunorj@gmail.com>
2023-11-06 20:02:48 -0300

    testando o recebimento do formulário

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+// Para o Express usar o EJS como view engine
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.set('view engine', 'ejs');
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+// Rotas
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.get('/perguntar', (req, res) => {
   res.render('perguntar');
+});
+
+app.post('/salvarPergunta', (req, res) => {
+  let titulo = req.body.titulo;
+  let descricao = req.body.descricao;
+  res.send(
+    `Formulário recebido! Título: ${titulo} Descrição: ${descricao}`
+  );
 });
 
 app.listen(3000, () => {
