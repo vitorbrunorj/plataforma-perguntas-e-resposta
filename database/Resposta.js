43bc0c93625e2124181b7ecc676a2464a98ca664
Vitor Bruno <vitorbrunorj@gmail.com>
2023-11-07 11:30:57 -0300

    Criada a tabela de respostas

---
+++ database/Resposta.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+const connection = require('./database');
+
+const Resposta = connection.define('respostas', {
+  corpo: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  perguntaId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+});
+
+Resposta.sync({ force: false }).then(() => {
+  console.log('Tabela de resposta criada!');
+});
+
+module.exports = Resposta;
